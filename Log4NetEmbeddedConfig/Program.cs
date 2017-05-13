78c11ecbaa3d7e28e61432f4e7d314c04d121a5f
toy <toy@ownway.info>
2017-05-13 23:22:48 +0900

    初期コミット。

---
+++ Log4NetEmbeddedConfig/Program.cs
@@ -0,0 +1,29 @@
+﻿using log4net;
+using log4net.Config;
+
+using System;
+using System.Reflection;
+
+namespace Log4NetEmbeddedConfig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Assembly assembly = Assembly.GetExecutingAssembly();
+                XmlConfigurator.Configure(
+                    assembly.GetManifestResourceStream("Log4NetEmbeddedConfig.Config.log.xml")
+                );
+
+                ILog logger = LogManager.GetLogger(typeof(Program));
+                logger.Debug("Hello World");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
